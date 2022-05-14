59f326db64f92270cdc5f486769aca6cde089f1d
xfait <99980992+Faiter29@users.noreply.github.com>
2022-05-14 17:20:03 +0200

    PT10

---
+++ Fait_Martin_111221_zadanie10.cpp/cmake-build-debug/CMakeFiles/cvicenie11.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvicenie11.dir/main.cpp.obj"
+  "CMakeFiles/cvicenie11.dir/main.cpp.obj.d"
+  "cvicenie11.exe"
+  "cvicenie11.exe.manifest"
+  "cvicenie11.pdb"
+  "libcvicenie11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvicenie11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Fait_Martin_111221_zadanie10.cpp/cmake-build-debug/CMakeFiles/cvicenie11.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cvicenie11.
+# This may be replaced when dependencies are built.
