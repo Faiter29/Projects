43ae724ee118491e29198121fbdefe53c298eb00
xfait <99980992+Faiter29@users.noreply.github.com>
2022-05-14 17:16:37 +0200

    PT6

---
+++ Fait_Martin_111221_zadanie06.cpp/cmake-build-debug/CMakeFiles/Zadanie_6.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Programko/CLionProjects/Programovanie 2/Zadanie_6/Fait_Martin_111221_zadanie06.cpp" "CMakeFiles/Zadanie_6.dir/Fait_Martin_111221_zadanie06.cpp.obj" "gcc" "CMakeFiles/Zadanie_6.dir/Fait_Martin_111221_zadanie06.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Fait_Martin_111221_zadanie06.cpp/cmake-build-debug/CMakeFiles/Zadanie_6.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zadanie_6.dir/Fait_Martin_111221_zadanie06.cpp.obj"
+  "CMakeFiles/Zadanie_6.dir/Fait_Martin_111221_zadanie06.cpp.obj.d"
+  "Zadanie_6.exe"
+  "Zadanie_6.exe.manifest"
+  "Zadanie_6.pdb"
+  "libZadanie_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zadanie_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
