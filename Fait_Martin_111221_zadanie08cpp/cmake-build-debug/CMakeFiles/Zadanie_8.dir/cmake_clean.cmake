a0416b1fac48897deb51a9150792def0ef17ceae
xfait <99980992+Faiter29@users.noreply.github.com>
2022-05-14 17:18:28 +0200

    PT8

---
+++ Fait_Martin_111221_zadanie08cpp/cmake-build-debug/CMakeFiles/Zadanie_8.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zadanie_8.dir/main.cpp.obj"
+  "CMakeFiles/Zadanie_8.dir/main.cpp.obj.d"
+  "Zadanie_8.exe"
+  "Zadanie_8.exe.manifest"
+  "Zadanie_8.pdb"
+  "libZadanie_8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zadanie_8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
