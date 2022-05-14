ee4a740b3ec42231a85b36c20dda80b3dc11afc5
xfait <99980992+Faiter29@users.noreply.github.com>
2022-05-14 17:19:17 +0200

    PT9

---
+++ Fait_Martin_111221_zadanie09.cpp/cmake-build-debug/CMakeFiles/Zadanie_9.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zadanie_9.dir/main.cpp.obj"
+  "CMakeFiles/Zadanie_9.dir/main.cpp.obj.d"
+  "Zadanie_9.exe"
+  "Zadanie_9.exe.manifest"
+  "Zadanie_9.pdb"
+  "libZadanie_9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zadanie_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Fait_Martin_111221_zadanie09.cpp/cmake-build-debug/CMakeFiles/Zadanie_9.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Zadanie_9.
+# This may be replaced when dependencies are built.
