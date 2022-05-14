7c4527397cf0636f3874bb3b21b6b97ae71f7720
xfait <99980992+Faiter29@users.noreply.github.com>
2022-05-14 17:14:59 +0200

    PT4

---
+++ Fait_Martin_111221_zadanie04.cpp/cmake-build-debug/CMakeFiles/yadanie_4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yadanie_4.dir/Fait_Martin_111221_zadanie04.cpp.obj"
+  "CMakeFiles/yadanie_4.dir/Fait_Martin_111221_zadanie04.cpp.obj.d"
+  "libyadanie_4.dll.a"
+  "yadanie_4.exe"
+  "yadanie_4.exe.manifest"
+  "yadanie_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yadanie_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
